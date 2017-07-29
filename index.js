4181aeb65942f6c3401b561eb9740300e18953fa
Chris Waalberg <chris@q42.nl>
2017-07-29 22:20:21 +0200

    dit is eigenlijk best wel ruk

--- index.js
+++ index.js
@@ -1,23 +1,88 @@
 Vue.component('portfolio-item', {
   props: ['item'],
-  template: '<article><h1>{{ item.title }}</h1></article>'
+  template: '\
+    <article>\
+      <span class="title-container">\
+        <h2 class="show-on-load" :class="{ streamer : !item.streamer }">{{item.title}}</h2>\
+        <h3 v-if="item.streamer" class="streamer show-on-load">{{item.streamer}}</h3>\
+      </span>\
+      <div v-for="image in item.images" class="image-container show-on-load" :class="image.type">\
+        <img v-if="image.type != \'custom\'" :src="\'images/\' + image.type + \'.png\'" class="device">\
+        <img :src="image.src" class="content">\
+      </div>\
+      <div class="background show-on-load" :style="\'background-image: url(\' + item.backgroundImage + \')\'"></div>\
+    </article>\
+  '
 });
+
+// portfolioItem:
+//   title: case title
+//   streamer: streamer text
+//   images: array of objects; ordered large to small
+//     src: source url
+//     type: desktop, tablet, phone or custom
+//   callToAction: array of objects
+//     title: link text
+//     url: link href
 
 var vm = new Vue({
   el: '#app',
   data: {
     portfolioItems: [
       {
-        id: 1,
-        title: 'PostNL app'
+        title: 'Rijksmuseum.nl',
+        streamer: 'Zelf aan de slag met 600.000 meesterwerken',
+        backgroundImage: 'images/portfolio/rijksmuseumnl-desktop.jpg',
+        images: [
+          {
+            src: 'images/portfolio/rijksmuseumnl-desktop.jpg',
+            type: 'desktop'
+          },
+          {
+            src: 'images/portfolio/rijksmuseumnl-tablet.jpg',
+            type: 'tablet'
+          },
+          {
+            src: 'images/portfolio/rijksmuseumnl-phone.jpg',
+            type: 'phone'
+          }
+        ],
+        callToAction: [
+          {
+            title: 'Ga naar Rijksmuseum.nl',
+            url: 'http://rijksmuseum.nl'
+          }, {
+            title: 'Meer over Rijksmuseum.nl',
+            url: '/portfolio/rijksmuseum.nl'
+          }
+        ]
       },
       {
-        id: 2,
-        title: 'MuseumExplorer app'
+        title: 'MuseumExplorer app',
+        streamer: 'De museumgids in je broekzak',
+        images: [
+          {
+            src: 'images/portfolio/museumexplorer-phone.png',
+            type: 'phone'
+          }
+        ],
+        callToAction: [{
+          title: 'Meer over de MuseumExplorer app',
+          url: '/blog/museumexplorer'
+        }]
       },
       {
-        id: 3,
-        title: 'Rijksmuseum.nl'
+        title: 'Philips Hue cloud platform',
+        images: [
+          {
+            src: 'images/portfolio/philipshue.png',
+            type: 'custom'
+          }
+        ],
+        callToAction: [{
+          title: 'Meer over Philips Hue',
+          url: '/portfolio/philipshue'
+        }]
       }
     ]
   }
